3327248dd693979219a7126282d93a70e4ec36af
Gerardo A Lopez <mcgalv@gmail.com>
2019-08-17 13:20:44 -0500

    TZ None

--- turismo/settings.py
+++ turismo/settings.py
@@ -162,7 +162,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-TIME_ZONE = 'America/Mexico_City'
+TIME_ZONE = None
+# TIME_ZONE = 'America/Mexico_City'
  
 
 # Static files (CSS, JavaScript, Images)
